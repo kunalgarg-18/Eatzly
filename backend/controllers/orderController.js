f2d53c44a6634509670794fb1b824e29a1a11366
Kunal Garg <imgargk18@gmail.com>
2024-08-10 19:17:27 +0530

    Orders status update and listing on admin panel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async(req,res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "http://localhost:5174"
 
     try {
         const newOrder = new orderModel({
@@ -86,5 +86,29 @@ const userOrders = async(req,res) => {
     }
 }
 
+//Listing orders for admin panel
 
-export {placeOrder, verifyOrder, userOrders}
+const listOrders = async(req,res) => {
+    try {
+        const orders =  await orderModel.find({});
+        res.json({success:true, data: orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "error"})
+    }
+}
+
+// api for updating order status
+
+const updateStatus = async(req,res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success: true, message: "Status Updated"})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "error"})
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus}
